711a4228bfec235c63ecc0b6f7bd2bef73c88630
Max <maximku97@gmail.com>
2026-08-03 20:37:15 +0300

    practice

---
+++ src/components/App/App.jsx
@@ -0,0 +1,34 @@
+import Header from "../Header/Header";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
+import Reviews from "../Reviews/Reviews";
+import AboutMe from "../AboutMe/AboutMe";
+import AboutUs from "../AboutUs/AboutUs";
+import Contact from "../AboutMe/Contact";
+import Hobbies from "../AboutMe/Hobbies";
+import MyStory from "../AboutMe/MyStory";
+import SiteHistory from "../AboutUs/SiteHistory";
+import SiteMission from "../AboutUs/SiteMission";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/about-me" element={<AboutMe />}>
+          <Route path="contact" element={<Contact />} />
+          <Route path="hobbies" element={<Hobbies />} />
+          <Route path="my-story" element={<MyStory />} />
+        </Route>
+        <Route path="/about-us" element={<AboutUs />}>
+          <Route path="history" element={<SiteHistory />} />
+          <Route path="mission" element={<SiteMission />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"; // New import
+import "./Dashboard.css";
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <h1>Emoji Critic — All About Emojis</h1>
+      <p>
+        The #1 Destination for Emoji Reviews on the World Wide Web Since 2020!
+      </p>
+      {/* Add a Link tag that links to our /reviews route. */}
+      <Link to="/reviews">Click here to see my latest reviews!</Link>
+    </div>
+  );
+}
+
+export default Dashboard;
