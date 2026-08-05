6917c440c233bc7937632f8afe26c20187c657d5
Max <maximku97@gmail.com>
2026-08-05 14:10:15 +0300

    Fix routing and CSS import bugs in reviews and about pages

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,9 +1,25 @@
-function Reviews() {
+import "./Reviews.css";
+import { useParams } from "react-router-dom";
+
+function Review({ reviews }) {
+  const params = useParams();
+  let id = params.reviewId;
+  // Decrement the id from the parameter so we access the correct items. This
+  // is necessary because the array indexes start with 0, whereas the IDs in
+  // the API begin at 1.
+  id = id - 1;
+
   return (
-    <div>
-      <p>My reviews will go here!</p>
+    <div className="review">
+      {reviews && (
+        <div className="review__item">
+          <h3>{reviews[id]?.title}</h3>
+          <p>{reviews[id]?.text}</p>
+          <p className="review__rating">Final rating:{reviews[id]?.rating}/5</p>
+        </div>
+      )}
     </div>
   );
 }
 
-export default Reviews;
+export default Review;
